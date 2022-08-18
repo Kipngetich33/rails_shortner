210aaa0da6f32e0f81196e4c952705deb463944d
Kipngetich33 <khalifngeno@gmail.com>
2022-08-18 18:37:06 +0300

    add the different url routes

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -1,7 +1,63 @@
 class Api::V1::UrlsController < ApplicationController
   def index
+    '''
+    Show all the Urls in the database
+    '''
+    puts("*"*80)
+    puts("fetching records")
+    urls = Url.all()
+    render json: urls, status: 200
+  end
+
+  def create
+    '''
+    Method that creates a new Url record
+    '''
+    puts("*"*80)
+    puts("creating object")
+    url = Url.new(
+      httpurl: url_params[:httpurl],
+      short_id: url_params[:short_id]
+    ) 
+    # now save the url
+    if url.save()
+      render json: url , status: 200
+    else
+      render json: {error: "Error occured while saving url."}
+    end
   end
 
   def show
+    '''
+    Method that looks up a URL using its id and renders it else
+    renders an error
+    '''
+    url = Url.find_by(id: params[:id])
+    if url
+      render json: url, status: 200
+    else 
+      render json: {error: "Url not found."}
+    end
   end
+
+  def other
+    render json: {status:true}
+  end
+
+  private
+    '''
+    Define private methods here to allow specific parameters to
+    be passed to class
+    '''
+    
+    def url_params
+      '''
+      define the specific model columns here
+      '''
+      params.require(:url).permit([
+        :httpurl, 
+        :short_id
+      ])
+    end
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Url.create([
+    {
+        http_url:"https://www.google.com/search?q=what+is+a+401k&oq=what+is+a+401k&aqs=chrome..69i57j0i512l5j0i10i512j0i512l3.8077j0j7&sourceid=chrome&ie=UTF-8",
+        short_code:"xyz2"
+    },
+    {
+        http_url:"https://stackoverflow.com/questions/45474191/an-error-occurred-while-installing-mysql2-0-4-8-and-bundler-cannot-continue",
+        short_code:"xyz3"
+    },
+    {
+        http_url:"https://github.com/capistrano/capistrano/issues/1953",
+        short_code:"xyz4"  
+    },
+    {
+        http_url:"https://stackoverflow.com/questions/25168791/creating-a-ruby-gem-require-error-cannot-load-such-file-loaderror",
+        short_code:"xyz5"  
+    },
+    {
+        http_url:"https://dev.to/mikerogers0/fixing-sqlite3-gem-which-is-not-supported-on-heroku-2mmp",
+        short_code:"xyz6"   
+    }
+])
