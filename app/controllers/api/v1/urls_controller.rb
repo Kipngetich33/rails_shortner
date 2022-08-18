c53c918dd7000e2e9f5e2b33ab8c6232f6918a58
Kipngetich33 <khalifngeno@gmail.com>
2022-08-18 18:47:44 +0300

    fix issue causing posts to fail

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -3,8 +3,6 @@ class Api::V1::UrlsController < ApplicationController
     '''
     Show all the Urls in the database
     '''
-    puts("*"*80)
-    puts("fetching records")
     urls = Url.all()
     render json: urls, status: 200
   end
@@ -13,11 +11,9 @@ class Api::V1::UrlsController < ApplicationController
     '''
     Method that creates a new Url record
     '''
-    puts("*"*80)
-    puts("creating object")
     url = Url.new(
-      httpurl: url_params[:httpurl],
-      short_id: url_params[:short_id]
+      http_url: url_params[:http_url],
+      short_code: url_params[:short_code]
     ) 
     # now save the url
     if url.save()
@@ -55,9 +51,9 @@ class Api::V1::UrlsController < ApplicationController
       define the specific model columns here
       '''
       params.require(:url).permit([
-        :httpurl, 
-        :short_id
+        :http_url, 
+        :short_code
       ])
     end
-    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # get 'urls/index'
-      # get 'urls/show'
-      resources :urls, only: [:index, :show, :create]
+      get 'urls/index'
+      get 'urls/show'
+      post 'urls/create'
+      # post not working below
+      # resources :urls, only: [:index, :show, :create]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
