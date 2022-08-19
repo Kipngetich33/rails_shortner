43ae6c4770b29ebff6ee3de82facdfc69c05c0bc
Kipngetich33 <khalifngeno@gmail.com>
2022-08-19 10:30:16 +0300

    add functionality to generate unique key and ensure that only unique URLs are saved in the database

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -4,22 +4,32 @@ class Api::V1::UrlsController < ApplicationController
     Show all the Urls in the database
     '''
     urls = Url.all()
-    render json: urls, status: 200
+    return render json: urls, status: 200
   end
 
   def create
     '''
     Method that creates a new Url record
-    '''
+    '''    
+    # generate unique code
+    new_short_code = generate_unique_key()
+
+    # check uniqueness of url
+    @current_url = url_params[:http_url]
+    unless check_if_url_is_unique()
+      return render json: {error: "Url already exists in database."}
+    end
+
+    # create a new Url instance
     url = Url.new(
       http_url: url_params[:http_url],
-      short_code: url_params[:short_code]
+      short_code: new_short_code
     ) 
     # now save the url
     if url.save()
-      render json: url , status: 200
+      return render json: url , status: 200
     else
-      render json: {error: "Error occured while saving url."}
+      return render json: {error: "Error occured while saving url."}
     end
   end
 
@@ -30,14 +40,42 @@ class Api::V1::UrlsController < ApplicationController
     '''
     url = Url.find_by(id: params[:id])
     if url
-      render json: url, status: 200
+      return render json: url, status: 200
     else 
-      render json: {error: "Url not found."}
+      return render json: {error: "Url not found."}
     end
   end
 
-  def other
-    render json: {status:true}
+  def generate_unique_key
+    '''
+    Method that generates a unique key to be used a s
+    shortcode
+    '''
+    # generate a shortcode 6 character long
+    generated_shortcode = SecureRandom.hex(3)
+    # check that key is not already in database
+    new_code = Url.find_by(short_code: generated_shortcode)
+    if new_code
+      # recursive call the function to create a new code
+      generate_unique_key()
+    else
+      # return valid code
+      return generated_shortcode
+    end
+  end
+
+  def check_if_url_is_unique
+    '''
+    Method that checks whether the given url is unique
+    or already exists in the database and returns appropriate 
+    message
+    '''
+    url = Url.find_by(http_url: @current_url)
+    if url
+      return false
+    else
+      return true
+    end
   end
 
   private
